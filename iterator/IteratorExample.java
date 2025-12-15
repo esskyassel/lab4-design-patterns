8900581cc9d74284fc05235459490586d884f8d8
esskyassel <kyassellessel@gmail.com>
2025-12-16 01:06:41 +0300

    Create IteratorExample.java

---
+++ iterator/IteratorExample.java
@@ -0,0 +1,34 @@
+interface MyIterator<T> {
+    boolean hasNext();
+    T next();
+}
+
+class ArrayIterator<T> implements MyIterator<T> {
+    private T[] items;
+    private int position = 0;
+
+    public ArrayIterator(T[] items) {
+        this.items = items;
+    }
+
+    public boolean hasNext() {
+        return position < items.length;
+    }
+
+    public T next() {
+        return items[position++];
+    }
+}
+
+public class IteratorExample {
+    public static void main(String[] args) {
+        String[] data = {"A", "B", "C"};
+
+        MyIterator<String> iterator = new ArrayIterator<>(data);
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
+дрльше
